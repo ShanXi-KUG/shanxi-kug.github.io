50f0fec6ff3139b50048fd6e040c1fe4c9ac1ce2
heke <chengkelfan@qq.com>
2024-12-24 20:42:40 +0800

    Fixed: fixed pug-vite-ts plugin error

--- publish-page.sh
+++ publish-page.sh
@@ -4,5 +4,6 @@ git fetch --all
 git checkout release || git checkout -b release origin/release
 git merge --no-commit origin/main
 
-git commit -m "Merged: main into release"
 git push origin release
+
+git checkout main

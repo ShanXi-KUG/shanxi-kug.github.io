f74cce00cd75f4526471689a544503a1a923d5a8
heke <chengkelfan@qq.com>
2024-12-24 20:11:49 +0800

    Create: publish-page.sh to merge main branch to release

--- publish-page.sh
+++ publish-page.sh
@@ -1,7 +1,7 @@
 git pull
 git fetch --all
 
-git checkout -b release origin/release
+git checkout release || git checkout -b release origin/release
 git merge --no-commit origin/main
 
 git commit -m "Merged: main into release"
